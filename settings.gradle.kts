75767de1efa2027c1a8123fc1a62a19279da5f8f
Jason Đoàn <35907749+doananhtuan22111996@users.noreply.github.com>
2024-09-02 23:43:25 +0700

    feat: Create lib composex (#1)

---
+++ settings.gradle.kts
@@ -0,0 +1,51 @@
+import java.net.URI
+import java.util.Properties
+
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            val ghUsername = System.getenv("USERNAME") ?: getLocalProperty("USERNAME")
+            val ghPassword = System.getenv("TOKEN") ?: getLocalProperty("TOKEN")
+            url = uri("https://maven.pkg.github.com/${ghUsername}/REPOSITORY")
+            credentials {
+                username = ghUsername
+                password = ghPassword
+            }
+        }
+    }
+}
+
+
+fun getLocalProperty(propertyName: String): String {
+    val localProperties = Properties().apply {
+        val localPropertiesFile = File(rootDir, "local.properties")
+        if (localPropertiesFile.exists()) {
+            load(localPropertiesFile.inputStream())
+        }
+    }
+
+    return localProperties.getProperty(propertyName) ?: run {
+        throw NoSuchFieldException("Not defined property: $propertyName")
+    }
+}
+
+rootProject.name = "Components"
+include(":libx")
+include(":libx:composex")
