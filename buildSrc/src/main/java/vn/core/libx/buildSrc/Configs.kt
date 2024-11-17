39743ba0f04739dabd6affce4de92b8e8ad49770
jasondoan <doananhtuan22111996@gmail.com>
2024-11-17 12:16:05 +0700

    feat: migrate to kotlin 2xx

---
+++ buildSrc/src/main/java/vn/core/libx/buildSrc/Configs.kt
@@ -0,0 +1,11 @@
+package vn.core.libx.buildSrc
+
+object Configs {
+
+    object Artifact {
+        const val GROUP_ID = "vn.core.libs"
+        const val ARTIFACT_COMPOSE_ID = "compose"
+        const val ARTIFACT_MDC_ID = "mdc"
+        const val VERSION = "1.0.0"
+    }
+}
--- libx/composex/build.gradle.kts
+++ libx/composex/build.gradle.kts
@@ -1,94 +1,28 @@
+import vn.core.libx.buildSrc.Configs
+
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    id("maven-publish")
+    vn.core.plugins.androidLibrary
+    vn.core.plugins.androidCompose
+    vn.core.plugins.androidPublishing
 }
 
 android {
     namespace = "vn.core.libx.composex"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
 }
 
 dependencies {
-    implementation(libs.core.domain)
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.paging.compose)
-    implementation(libs.androidx.ui.text.google.fonts)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.domain)
 }
 
 publishing {
-    val ghUsername = System.getenv("USERNAME")
-    val ghPassword = System.getenv("TOKEN")
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/${ghUsername}/android-components")
-            credentials {
-                username = ghUsername
-                password = ghPassword
-            }
-        }
-    }
     publications {
-        create<MavenPublication>("mavenJava") {
+        create<MavenPublication>(Configs.Artifact.ARTIFACT_COMPOSE_ID) {
             afterEvaluate {
-                from(components["release"])
+                from(components["all"])
             }
-            groupId = "vn.core.libx-ui" // Replace with your GitHub username
-            artifactId = "composex"
-            version = "1.0.2" // Set your desired version here
+            groupId = Configs.Artifact.GROUP_ID // Replace with your GitHub username
+            artifactId = Configs.Artifact.ARTIFACT_COMPOSE_ID
+            version = Configs.Artifact.VERSION // Set your desired version here
         }
     }
 }
